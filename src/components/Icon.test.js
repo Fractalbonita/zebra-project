9da461e1c7dacd221f3ad9b2c509f2ccfd7b1af8
Kathrin Korhammer <kathrin.korhammer@gmail.com>
2020-09-05 18:07:07 +0200

    Add test for icon component

---
+++ src/components/Icon.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import Icon from './Icon';
+
+describe('Icon', () => {
+  it('renders a material icon', () => {
+    const { container } = render(<Icon type="bedtime" />);
+    expect(container.innerHTML).toBe(
+      '<span class="material-icons">bedtime</span>'
+    );
+  });
+
+  it('renders an icon of a given type', () => {
+    const { queryByText } = render(<Icon type="bedtime" />);
+    expect(queryByText('bedtime')).toBeTruthy();
+    expect(queryByText('code')).toBeFalsy();
+  });
+
+  it('renders props, e.g. calls the onClick callback handler', async () => {
+    const clickHandler = jest.fn();
+    const { getByText } = render(
+      <Icon type="bedtime" onClick={clickHandler} />
+    );
+    await fireEvent.click(getByText('bedtime'));
+    expect(clickHandler).toHaveBeenCalledTimes(1);
+  });
+});
