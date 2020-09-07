df2fe6a7fda1a3c5333b8a4a876fecd4f9210589
Kathrin Korhammer <kathrin.korhammer@gmail.com>
2020-09-08 00:06:03 +0200

    Add components to app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ModeSwitcher from './components/ModeSwitcher';
 import GlobalStyles from './styles/GlobalStyles';
+import ReminderForm from './components/ReminderForm';
+import { useLocalStorageForReminders } from './hooks/useLocalStorageForReminders';
+import ReminderList from './components/ReminderList';
 
 function App() {
+  const [reminders, addReminder] = useLocalStorageForReminders();
+
   return (
     <div className="app">
       <GlobalStyles />
       <h1>Hello Zebra!</h1>
       <ModeSwitcher />
+      <ReminderList reminders={reminders} />
+      <ReminderForm addReminder={addReminder} />
     </div>
   );
 }
