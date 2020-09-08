167c8465cb8d7ee9999949201c19e4d43992de90
Kathrin Korhammer <kathrin.korhammer@gmail.com>
2020-09-08 17:52:26 +0200

    Add primary color

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -3,33 +3,39 @@ import { createGlobalStyle } from 'styled-components';
 const darkTheme = {
   textColor: '#ffffff',
   surfaceColor: '#000000',
+  primaryColor: 'palegreen',
 };
 
 const lightTheme = {
   textColor: '#000000',
   surfaceColor: '#ffffff',
+  primaryColor: 'rosybrown',
 };
 
 export default createGlobalStyle`
 :root {
   --surface: ${lightTheme.surfaceColor};
   --text: ${lightTheme.textColor};
+  --primary: ${lightTheme.primaryColor};
 }
 
 body.switch-color-scheme {
   --surface: ${darkTheme.surfaceColor};
   --text: ${darkTheme.textColor};
+  --primary: ${darkTheme.primaryColor};
 }
 
 @media (prefers-color-scheme: dark) {
   :root {
     --surface: ${darkTheme.surfaceColor};
     --text: ${darkTheme.textColor};
+    --primary: ${darkTheme.primaryColor};
   }
 
   body.switch-color-scheme {
     --surface: ${lightTheme.surfaceColor};
     --text: ${lightTheme.textColor};
+    --primary: ${lightTheme.primaryColor};
   }
 }
 
