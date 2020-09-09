87902a0f5acfca0706c6cb8974b5bcadfd6ecd3e
Kathrin Korhammer <kathrin.korhammer@gmail.com>
2020-09-09 19:31:32 +0200

    Update tests in app

--- src/App.test.js
+++ src/App.test.js
@@ -1,23 +1,42 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+
+jest.mock('./components/ModeSwitcher', () => jest.fn());
+jest.mock('./components/ReminderList', () => jest.fn());
+jest.mock('./components/ReminderForm', () => jest.fn());
+
 import ModeSwitcher from './components/ModeSwitcher';
 import ReminderList from './components/ReminderList';
 import ReminderForm from './components/ReminderForm';
 
+ModeSwitcher.mockImplementation(() => <div />);
+ReminderList.mockImplementation(() => <div />);
+ReminderForm.mockImplementation(() => <div />);
+
 describe('App', () => {
   test('renders app', () => {
-    const renderedApp = render(<App />);
-    expect(renderedApp).toBeTruthy();
+    render(<App />);
   });
 
-  test('applies global styles', () => {
-    const { container } = render(<App />);
-    expect(container.querySelector('.app')).toHaveStyle(
-      'box-sizing: border-box',
-      'color: inherit',
-      'height: 100vh'
-    );
+  test('contains ModeSwitcher', () => {
+    const id = 'id' + Math.random();
+    ModeSwitcher.mockImplementation(() => <div data-testid={id} />);
+    const { getByTestId } = render(<App />);
+    expect(getByTestId(id)).toBeInTheDocument();
+  });
+  test('contains ReminderList', () => {
+    const id = 'id' + Math.random();
+    ReminderList.mockImplementation(() => <div data-testid={id} />);
+    const { getByTestId } = render(<App />);
+    expect(getByTestId(id)).toBeInTheDocument();
+  });
+
+  test('contains ReminderForm', () => {
+    const id = 'id' + Math.random();
+    ReminderForm.mockImplementation(() => <div data-testid={id} />);
+    const { getByTestId } = render(<App />);
+    expect(getByTestId(id)).toBeInTheDocument();
   });
 
   test('renders title', () => {
@@ -32,30 +51,12 @@ describe('App', () => {
     expect(title.tagName).toBe('H1');
   });
 
-  test('renders the ModeSwitcher component', () => {
-    const component = render(
-      <App>
-        <ModeSwitcher />
-      </App>
+  test('applies global styles', () => {
+    const { container } = render(<App />);
+    expect(container.querySelector('.app')).toHaveStyle(
+      'box-sizing: border-box',
+      'color: inherit',
+      'height: 100vh'
     );
-    expect(component).toBeTruthy;
-  });
-
-  test('renders the ReminderList component', () => {
-    const component = render(
-      <App>
-        <ReminderList />
-      </App>
-    );
-    expect(component).toBeTruthy;
-  });
-
-  test('renders the ReminderForm component', () => {
-    const component = render(
-      <App>
-        <ReminderForm />
-      </App>
-    );
-    expect(component).toBeTruthy;
   });
 });
