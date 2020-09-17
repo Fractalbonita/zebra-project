48ffab9cc84fe87a3ade56d44337011b2d16a175
Kathrin Korhammer <kathrin.korhammer@gmail.com>
2020-09-18 00:51:38 +0200

    Add datetimepicker component

---
+++ src/components/ReminderListItemSchedule.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import { DateTimePicker } from '@material-ui/pickers';
+import PropTypes from 'prop-types';
+
+ReminderListItemSchedule.propTypes = {
+  onClose: PropTypes.func,
+  onSchedule: PropTypes.func,
+};
+
+export default function ReminderListItemSchedule({ onClose, onSchedule }) {
+  const [date, setDate] = useState(new Date());
+  return (
+    <DateTimePicker
+      variant="dialog"
+      value={date}
+      onChange={setDate}
+      open={true}
+      onClose={onClose}
+      onAccept={onSchedule}
+      format="dd MMM yyyy"
+      disablePast={true}
+      maxDate="2025-01-01"
+      ampm={false}
+      TextFieldComponent={() => null}
+    />
+  );
+}
