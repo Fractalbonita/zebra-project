d9cad8b590abb97ecb5261df64afe4efbfede067
Kathrin Korhammer <kathrin.korhammer@gmail.com>
2020-09-20 21:25:08 +0200

    Fix focus color of menu button

--- src/components/DropDownMenu.js
+++ src/components/DropDownMenu.js
@@ -6,7 +6,7 @@ import { useMenuToggle } from '../hooks/useMenuToggle';
 import PropTypes from 'prop-types';
 
 DropDownMenu.propTypes = {
-  children: PropTypes.element.isRequired,
+  children: PropTypes.array.isRequired,
 };
 
 export default function DropDownMenu({ children }) {
@@ -31,9 +31,15 @@ export default function DropDownMenu({ children }) {
 
 const Container = styled.div`
   position: relative;
+
+  & button:focus {
+    background-color: isShown && var(--surface);
+  }
 `;
 
 const Items = styled.div`
+  background-color: var(--on-surface);
+  box-shadow: 0 1px 3px var(--light-shadow), 0 1px 2px var(--dark-shadow);
   position: absolute;
   top: 0%;
   right: 0;
