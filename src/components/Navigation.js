4c0fbaa4fb8b8ca6a12cfb07852f1d884c89bfbf
Kathrin Korhammer <kathrin.korhammer@gmail.com>
2020-09-24 00:26:30 +0200

    Structure navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import Icon from './Icon';
+
+const AppLink = ({ title, address, icon }) => (
+  <NavLink exact to={address} activeClassName="active">
+    <Icon icon={icon} />
+    {title}
+  </NavLink>
+);
 
 export default function Navigation() {
   return (
     <div>
-      <button>Lists</button>
-      <button>List</button>
+      <nav>
+        <ul>
+          <li>
+            <AppLink title="Collection" address="/" icon="list" />
+          </li>
+          <li>
+            <AppLink title="List" address="/reminderlist" icon="check" />
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }
