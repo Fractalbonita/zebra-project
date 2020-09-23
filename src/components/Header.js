82dabba251d75c783192e2154835cbdeb793f7c9
Kathrin Korhammer <kathrin.korhammer@gmail.com>
2020-09-23 23:05:40 +0200

    Create header componnet

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ModeSwitcher from './ModeSwitcher';
+
+export default function Header() {
+  return (
+    <div>
+      <h1>Hello Zebra!</h1>
+      <ModeSwitcher />
+    </div>
+  );
+}
