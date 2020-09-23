a49dcdf31615572e7a2aa5ff711932ab381be7ce
Kathrin Korhammer <kathrin.korhammer@gmail.com>
2020-09-23 15:50:07 +0200

    Delete obsolete useState import

--- src/App.js
+++ src/App.js
@@ -1,44 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ModeSwitcher from './components/ModeSwitcher';
-import ReminderForm from './components/ReminderForm';
-import { useLocalStorageForReminders } from './hooks/useLocalStorageForReminders';
-import ReminderList from './components/ReminderList';
-import ReminderListFilter from './components/ReminderListFilter';
 import { MuiPickersUtilsProvider } from '@material-ui/pickers';
 import DateFnsUtils from '@date-io/date-fns';
+import ReminderListView from './screens/ReminderListScreen';
 
 function App() {
-  const {
-    reminders,
-    addReminder,
-    toggleReminderState,
-    deleteReminder,
-    editReminder,
-    scheduleReminder,
-  } = useLocalStorageForReminders();
-
-  const [filter, setFilter] = useState('Active');
-
   return (
     <div className="app">
       <MuiPickersUtilsProvider utils={DateFnsUtils}>
         <h1>Hello Zebra!</h1>
         <ModeSwitcher />
-        <ReminderListFilter
-          filter={filter}
-          onFilter={(state) => {
-            setFilter(state);
-          }}
-        />
-        <ReminderList
-          reminders={reminders}
-          toggleReminderState={toggleReminderState}
-          deleteReminder={deleteReminder}
-          editReminder={editReminder}
-          filter={filter}
-          scheduleReminder={scheduleReminder}
-        />
-        <ReminderForm addReminder={addReminder} />
+        <ReminderListView />
       </MuiPickersUtilsProvider>
     </div>
   );
