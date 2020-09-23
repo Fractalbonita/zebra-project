4a668498429e9a8b0841c41247e7c3a785865778
Kathrin Korhammer <kathrin.korhammer@gmail.com>
2020-09-23 23:22:22 +0200

    Pre-style header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import styled from 'styled-components';
 import ModeSwitcher from './ModeSwitcher';
 
 export default function Header() {
   return (
-    <div>
+    <Container>
       <h1>Hello Zebra!</h1>
       <ModeSwitcher />
-    </div>
+    </Container>
   );
 }
+
+const Container = styled.div`
+  align-items: center;
+  display: grid;
+  grid-template-columns: auto 145px;
+`;
