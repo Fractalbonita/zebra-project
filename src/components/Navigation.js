a44b6df23fbc5f6794dd5aaa709aaad63e7ce844
Kathrin Korhammer <kathrin.korhammer@gmail.com>
2020-09-24 18:30:44 +0200

    Layout and style navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,13 +1,38 @@
 import React from 'react';
+import styled from 'styled-components';
 import NavigationLink from './NavigationLink';
 
 export default function Navigation() {
   return (
-    <nav>
+    <Nav>
       <ul>
-        <NavigationLink title="Collection" address="/" icon="list" />
+        <NavigationLink
+          title="Collection"
+          address="/"
+          icon="playlist_add_check"
+        />
         <NavigationLink title="List" address="/reminderlist" icon="check" />
       </ul>
-    </nav>
+    </Nav>
   );
 }
+
+const Nav = styled.nav`
+  align-items: center;
+  background-color: var(--surface);
+  bottom: 0;
+  display: grid;
+  grid-template-columns: 1fr;
+  height: 60px;
+  left: 0;
+  position: fixed;
+  right: 0;
+
+  ul {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+`;
--- src/components/NavigationLink.js
+++ src/components/NavigationLink.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import Icon from './Icon';
 
 NavigationLink.propTypes = {
@@ -12,10 +13,30 @@ NavigationLink.propTypes = {
 export default function NavigationLink({ title, address, icon }) {
   return (
     <li>
-      <NavLink exact to={address} activeClassName="active">
+      <Link exact to={address} activeClassName="active">
         <Icon icon={icon} />
         {title}
-      </NavLink>
+      </Link>
     </li>
   );
 }
+
+const Link = styled(NavLink)`
+  color: var(--accent);
+  display: flex;
+  flex-direction: column;
+  height: 48px;
+  justify-content: center;
+  outline: none;
+  text-align: center;
+  text-decoration: none;
+
+  &.active {
+    color: var(--primary);
+    font-weight: 900;
+  }
+`;
+
+// margin: 0;
+// padding: 0 5px;
+// text-transform: uppercase;
