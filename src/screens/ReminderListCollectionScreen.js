571cc7e308d21a395ea5d7f4684ddea2d015188c
Kathrin Korhammer <kathrin.korhammer@gmail.com>
2020-09-28 22:22:51 +0200

    Add reminderlistform

--- src/screens/ReminderListCollectionScreen.js
+++ src/screens/ReminderListCollectionScreen.js
@@ -1,5 +1,22 @@
 import React from 'react';
+import ReminderListForm from '../components/ReminderListForm';
+import { useLocalStorageForReminderList } from '../hooks/useLocalStorageForReminderList';
 
 export default function ReminderListCollectionScreen() {
-  return <h2>Lists</h2>;
+  const [
+    reminderList,
+    addReminderListTitle,
+    addReminder,
+    toggleReminderState,
+    deleteReminder,
+    editReminder,
+    scheduleReminder,
+  ] = useLocalStorageForReminderList();
+
+  return (
+    <>
+      <h2>Lists</h2>
+      <ReminderListForm addReminderListTitle={addReminderListTitle} />
+    </>
+  );
 }
