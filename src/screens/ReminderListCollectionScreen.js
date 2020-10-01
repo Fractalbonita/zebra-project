6a131d8c8e587c50ac283b21e0093ec3161ed155
Kathrin Korhammer <kathrin.korhammer@gmail.com>
2020-10-01 16:55:57 +0200

    Simplify component by removing boilerplate code

--- src/screens/ReminderListCollectionScreen.js
+++ src/screens/ReminderListCollectionScreen.js
@@ -1,22 +1,13 @@
 import React from 'react';
+import ReminderListCollection from '../components/ReminderListCollection';
 import ReminderListForm from '../components/ReminderListForm';
-import { useLocalStorageForReminderList } from '../hooks/useLocalStorageForReminderList';
 
 export default function ReminderListCollectionScreen() {
-  const [
-    reminderList,
-    addReminderListTitle,
-    addReminder,
-    toggleReminderState,
-    deleteReminder,
-    editReminder,
-    scheduleReminder,
-  ] = useLocalStorageForReminderList();
-
   return (
     <>
       <h2>Lists</h2>
-      <ReminderListForm addReminderListTitle={addReminderListTitle} />
+      <ReminderListForm />
+      <ReminderListCollection />
     </>
   );
 }
